b75edc5bc6fb76e8e6e75afbbdcf78ab19336eac
nadiaz2 <nadiaz2@wpi.edu>
2023-11-07 12:52:46 -0500

    Add random wait at start and understand observation info

--- src/agents/turingMario/Agent.java
+++ src/agents/turingMario/Agent.java
@@ -5,16 +5,38 @@ import engine.core.MarioForwardModel;
 import engine.core.MarioTimer;
 import engine.helper.MarioActions;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Agent implements MarioAgent {
+	private long startTime;
+	private double waitDuration;
+
 	@Override
 	public void initialize(MarioForwardModel model, MarioTimer timer) {
+		double minWait = 0.0;
+		double maxWait = 3.0;
+		Random generator = new Random(System.currentTimeMillis());
+		waitDuration = minWait + generator.nextDouble() * (maxWait - minWait);
 
+		startTime = model.getRemainingTime();
 	}
 
 	@Override
 	public boolean[] getActions(MarioForwardModel model, MarioTimer timer) {
 		boolean[] actionArray = new boolean[MarioActions.numberOfActions()];
-		actionArray[MarioActions.RIGHT.getValue()] = true;
+
+		//System.out.println(Arrays.deepToString(model.getMarioCompleteObservation()));
+		int[][] observation = model.getMarioCompleteObservation();
+		//observation[x][y] y increases downwards
+
+		int[] marioPos = model.getMarioScreenTilePos();
+		int marioX = marioPos[0];
+		int marioY = marioPos[1];
+
+		actionArray[MarioActions.RIGHT.getValue()] =
+				(startTime - model.getRemainingTime()) >= waitDuration*1000;
+
 		return actionArray;
 	}
 
