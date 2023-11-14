059f791a03170917f840575aa1544013e83b7edb
nadiaz2 <nadiaz2@wpi.edu>
2023-11-14 14:05:19 -0500

    Create first level generator file

---
+++ src/levelGenerators/DiazHariyantoLin/LevelGenerator.java
@@ -0,0 +1,18 @@
+package levelGenerators.DiazHariyantoLin;
+
+import engine.core.MarioLevelGenerator;
+import engine.core.MarioLevelModel;
+import engine.core.MarioTimer;
+
+public class LevelGenerator implements MarioLevelGenerator {
+
+	@Override
+	public String getGeneratedLevel(MarioLevelModel model, MarioTimer timer) {
+		return null;
+	}
+	
+	@Override
+	public String getGeneratorName() {
+		return "DiazHariyantoLinLevelGenerator";
+	}
+}
